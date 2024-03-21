f8be93509ee284d93d7fe898b61ab4e9929b2d04
DaniarTurgunov <tbylat@gmail.com>
2024-03-21 06:34:05 +0500

    hw6

---
+++ hw6/vue-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <OurProject />
+
+  </div>
+</template>
+
+<script>
+import OurProject from './Page/OurProject.vue';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    OurProject
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$width_site: 1200px;
+
+* {
+    margin: 0;
+    padding: 0;
+    
+}
+
+.center {
+    padding-left: calc(50% - $width_site / 2);
+    padding-right: calc(50% - $width_site / 2);
+}
+
+</style>
---
+++ hw6/vue-project/src/components/pages/BlogDetailsPage/BlogDetailsHeaderBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="blogdetailshead"></div>
+</template>
+
+<script>
+export default {
+  name: 'BlogDetailsHeaderBox',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+    .blogdetailshead{
+        background-image: url("../../../assets/images/blogdetailheader.svg") ;
+        height: 356px;
+        margin-bottom: 200px;
+    }
+</style>
